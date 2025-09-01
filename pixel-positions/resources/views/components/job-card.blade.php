6467b0e9595ef3a7d6c375da3f175c1901486efa
Arjun Das P <arjundaspalappilli@gmail.com>
2025-09-01 18:00:33 +0530

    Laracasts project - Pixel Positions

---
+++ pixel-positions/resources/views/components/job-card.blade.php
@@ -0,0 +1,18 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center border border-transparent hover:border-blue-800 group transition-colors duration-300">
+    <div class="self-start text-sm">Laracasts</div>
+
+    <div class="py-8">
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full Time - From $60,000</p>
+    </div>
+
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+
+        <X-employer-logo :width="42"/>
+    </div>
+</div>
